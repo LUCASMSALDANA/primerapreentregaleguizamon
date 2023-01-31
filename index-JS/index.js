ffbec77005bbbf31c45abecafeb78e0dccc0954a
Lucas <lucasmsaldana@gmail.com>
2023-01-31 17:11:31 -0300

    Consumimos una API de productos en el Index

--- index-JS/index.js
+++ index-JS/index.js
@@ -11,10 +11,6 @@ botonFinalizarcompra.addEventListener("click",finalizarCompra);
 
 const boton_compra= document.querySelectorAll(".botonCompra");
 
-for(let i=0; i<boton_compra.length;i++){
-    boton_compra[i].addEventListener("click",agregar_carrito)
-}
-
 actualizarCarrito();
 
 let url="https://dummyjson.com/products?limit=6";
@@ -29,8 +25,6 @@ function mostrarProducto(producto){
     console.log(producto);
 
     const areaMarket = document.getElementById("areaMarket")
-    const cartaColumna= document.createElement("div");
-    cartaColumna.className="carta-columna";
     for(let i = 0; i<producto.length;i++){
         const card= document.createElement("div")
         card.className="card";
@@ -46,12 +40,7 @@ function mostrarProducto(producto){
           <a href="#carrito" class="btn btn-primary botonCompra">Comprar</a>
         </div>
          `; 
-        cartaColumna.append(card);
-        if(i%2==0 & i!=0){
-            console.log(i);
-            areaMarket.append(cartaColumna);
-            i=999
-            
-        }
+
+        areaMarket.append(card);
     }
 }
